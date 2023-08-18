cb7d3becdbc3377c14550812d3888a3d907007e1
Siegfred Gamboa <siegy.gamboa@gmail.com>
2023-08-18 04:32:18 +0300

    refactor home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,41 @@
 import React from 'react';
 import { Container } from './page.styles';
+import {
+  LoginButton,
+  RegisterButton,
+  LogoutButton,
+  ProfileButton,
+} from '@/components/buttons/Buttons';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/lib/auth';
+import { User } from '@/components/user/User';
 
-const Home = () => {
-  return <Container>Home</Container>;
+const Home = async () => {
+  const session = await getServerSession(authOptions);
+  console.log(session);
+  return (
+    <Container>
+      {/* <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          flexDirection: 'column',
+          alignItems: 'center',
+          height: '70vh',
+        }}
+      >
+        <LoginButton />
+        <RegisterButton />
+        <LogoutButton />
+        <ProfileButton />
+
+        <h1>Server Session</h1>
+        <pre>{JSON.stringify(session)}</pre>
+
+        <User />
+      </div> */}
+      HOME
+    </Container>
+  );
 };
-
 export default Home;
