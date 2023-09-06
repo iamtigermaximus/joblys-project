8ac7d2da83599e5b5bfca1237530a67d7d6e9027
Siegfred Gamboa <siegy.gamboa@gmail.com>
2023-09-06 05:39:00 +0300

    refactor chat interface

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,6 +1,6 @@
 // pages/chat.tsx
 import React, { useState } from 'react';
-import useClient from '../../utils/useClient'; // Adjust the import path
+//import useClient from '../../utils/useClient'; // Adjust the import path
 import {
   AnswerContainer,
   Button,
@@ -13,50 +13,50 @@ import {
 } from './Chat.styles';
 
 const Chat: React.FC = () => {
-  const [messages, setMessages] = useState<{ text: string; isUser: boolean }[]>(
-    []
-  );
-  const [input, setInput] = useState<string>('');
+  // const [messages, setMessages] = useState<{ text: string; isUser: boolean }[]>(
+  //   []
+  // );
+  // const [input, setInput] = useState<string>('');
 
-  const client = useClient(
-    {
-      onConnect: () => {
-        console.log('Connected to server');
-        // When connected, you can start with an initial question
-        setMessages((prevMessages) => [
-          ...prevMessages,
-          {
-            text: 'What is your first name?',
-            isUser: false,
-          },
-        ]);
-      },
-      onChatResponse: (data) => {
-        console.log('Received chat response:', data);
-        setMessages((prevMessages) => [
-          ...prevMessages,
-          { text: data.message, isUser: false },
-        ]);
-      },
-      onDisconnect: () => {
-        console.log('Disconnected from server');
-      },
-    },
-    '/'
-  ); // Replace with your server URL
+  // const client = useClient(
+  //   {
+  //     onConnect: () => {
+  //       console.log('Connected to server');
+  //       // When connected, you can start with an initial question
+  //       setMessages((prevMessages) => [
+  //         ...prevMessages,
+  //         {
+  //           text: 'What is your first name?',
+  //           isUser: false,
+  //         },
+  //       ]);
+  //     },
+  //     onChatResponse: (data) => {
+  //       console.log('Received chat response:', data);
+  //       setMessages((prevMessages) => [
+  //         ...prevMessages,
+  //         { text: data.message, isUser: false },
+  //       ]);
+  //     },
+  //     onDisconnect: () => {
+  //       console.log('Disconnected from server');
+  //     },
+  //   },
+  //   '/'
+  // ); // Replace with your server URL
 
-  const handleSendMessage = async () => {
-    if (input.trim() === '') {
-      return;
-    }
+  // const handleSendMessage = async () => {
+  //   if (input.trim() === '') {
+  //     return;
+  //   }
 
-    client.sendMessage(input);
-    setMessages((prevMessages) => [
-      ...prevMessages,
-      { text: input, isUser: true },
-    ]);
-    setInput('');
-  };
+  //   client.sendMessage(input);
+  //   setMessages((prevMessages) => [
+  //     ...prevMessages,
+  //     { text: input, isUser: true },
+  //   ]);
+  //   setInput('');
+  // };
 
   return (
     <Container>
@@ -101,11 +101,11 @@ const Chat: React.FC = () => {
         <InputContainer>
           <Input
             type="text"
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
+            // value={input}
+            // onChange={(e) => setInput(e.target.value)}
             placeholder="Type your message..."
           />
-          <Button onClick={handleSendMessage}>Send</Button>
+          <Button>Send</Button>
         </InputContainer>
       </ChatbotContainer>
     </Container>
