aba0dbd2c78a4b0fb6d11599ddc0907470dacb9c
Siegfred Gamboa <siegy.gamboa@gmail.com>
2023-09-06 12:38:56 +0300

    refactor profile page, hardcoded sample data to show interface

--- src/components/account/Account.styles.tsx
+++ src/components/account/Account.styles.tsx
@@ -8,7 +8,6 @@ export const Container = styled.div`
   height: 100%;
   border-radius: 20px;
   /* border: 1px solid red; */
-
   padding: 30px 100px;
 `;
 
@@ -27,9 +26,7 @@ export const ProfileSection = styled.div`
   display: flex;
   /* border: 1px solid red; */
   width: 100%;
-  height: 150px;
-  border: 1px solid #e0e0e0;
-  background-color: #ffffff;
+  height: 100%;
   box-shadow: 0 0 10px white;
 `;
 
@@ -55,7 +52,7 @@ export const ResumeDocumentSection = styled.div`
   display: flex;
   /* border: 1px solid red; */
   width: 100%;
-  height: 150px;
+  height: 100%;
   margin: 10px 0;
   border: 1px solid #e0e0e0;
   background-color: #ffffff;
@@ -66,7 +63,7 @@ export const SummarySection = styled.div`
   display: flex;
   /* border: 1px solid red; */
   width: 100%;
-  height: 150px;
+  height: 100%;
   margin: 10px 0;
   border: 1px solid #e0e0e0;
   background-color: #ffffff;
@@ -101,7 +98,7 @@ export const SkillsSection = styled.div`
   flex-direction: column;
   /* border: 1px solid red; */
   width: 100%;
-  height: 150px;
+  height: 100%;
   margin: 10px 0;
   border: 1px solid #e0e0e0;
   background-color: #ffffff;
@@ -112,20 +109,127 @@ export const ExperienceSection = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
-  height: 150px;
+  height: 100%;
   margin: 10px 0;
   border: 1px solid #e0e0e0;
   background-color: #ffffff;
   box-shadow: 0 0 10px white;
+  padding: 10px;
 `;
 
 export const EducationSection = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
-  height: 150px;
+  height: 100%;
   margin: 10px 0;
   border: 1px solid #e0e0e0;
   background-color: #ffffff;
   box-shadow: 0 0 10px white;
+  padding: 10px;
+`;
+
+export const SectionContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+  height: 100%;
+  width: 100%;
+`;
+
+export const SectionHeading = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 3px;
+`;
+
+export const SectionHeadingText = styled.h1`
+  font-size: 13px;
+  color: #232946;
+`;
+
+export const SectionHeadingIcon = styled.span`
+  font-size: 16px;
+  color: #232946;
+  margin: 0 5px;
+`;
+
+export const SectionBody = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 3px;
+  margin: 10px 0;
+  font-size: 13px;
+  color: #232946;
+`;
+
+export const UploadedResumeContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 10px 5px;
+  background-color: #e8e8e8;
+  width: 100%;
+`;
+
+export const LastUpdated = styled.h1`
+  padding: 5px;
+  color: gray;
+  font-size: 8px;
+`;
+
+export const SummarySectionText = styled.p`
+  font-size: 13px;
+  color: #232946;
+  margin-right: 100px;
+  font-weight: 500;
+`;
+
+export const SkillsWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 5px;
+  flex-wrap: wrap;
+`;
+
+export const SkillContainer = styled.div`
+  background-color: #e8e8e8;
+  width: 100px;
+  font-size: 13px;
+  padding: 5px 10px;
+  margin: 5px 10px;
+  border-radius: 5px;
+`;
+
+export const AddLabelContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const AddButton = styled.button`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #e8e8e8;
+  padding: 5px;
+`;
+
+export const Label = styled.h1`
+  display: flex;
+  align-items: center;
+  font-size: 16px;
+`;
+
+export const WorkSectionContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+  height: 100%;
+  width: 100%;
+  background-color: #e8e8e8;
+  margin: 10px 0;
 `;
--- src/components/account/Account.tsx
+++ src/components/account/Account.tsx
@@ -1,42 +1,270 @@
 'use client';
 import React from 'react';
 import {
+  AddButton,
+  AddLabelContainer,
   ContactInformation,
   Container,
   EducationSection,
   ExperienceSection,
+  Label,
+  LastUpdated,
   LinkContainer,
   LinksSection,
   LocationInformation,
   ProfileContainer,
   ProfileSection,
   ResumeDocumentSection,
+  SectionBody,
+  SectionContent,
+  SectionHeading,
+  SectionHeadingIcon,
+  SectionHeadingText,
+  SkillContainer,
   SkillsSection,
+  SkillsWrapper,
   SummarySection,
+  SummarySectionText,
+  UploadedResumeContainer,
+  WorkSectionContent,
 } from './Account.styles';
+import {
+  HiPencil,
+  HiTrash,
+  HiDocumentArrowDown,
+  HiCalendarDays,
+  HiOutlineLocationMarker,
+} from 'react-icons/hi2';
 
 const Account = () => {
   return (
     <Container>
       <ProfileContainer>
         <ProfileSection>
-          <ContactInformation>Contact Info</ContactInformation>
-          <LocationInformation>Location Info</LocationInformation>
+          <ContactInformation>
+            <SectionContent>
+              <SectionHeading>
+                <SectionHeadingText>Contact Information</SectionHeadingText>
+                <SectionHeadingIcon>
+                  <HiPencil />
+                </SectionHeadingIcon>
+              </SectionHeading>
+              <SectionBody>
+                <h4>Siegfred Gamboa</h4>
+                <h4>siegy.gamboa@example.com</h4>
+                <h4>+358902544642</h4>
+              </SectionBody>
+            </SectionContent>
+          </ContactInformation>
+          <LocationInformation>
+            <SectionContent>
+              <SectionHeading>
+                <SectionHeadingText>Location Information</SectionHeadingText>
+                <SectionHeadingIcon>
+                  <HiPencil />
+                </SectionHeadingIcon>
+              </SectionHeading>
+              <SectionBody>
+                <h4>Helsinki, 00100</h4>
+                <h4>Finland</h4>
+              </SectionBody>
+            </SectionContent>
+          </LocationInformation>
         </ProfileSection>
-        <ResumeDocumentSection>Resume document section</ResumeDocumentSection>
-        <h4>Summary</h4>
-        <SummarySection>Summary section</SummarySection>
-        <h4>Links</h4>
+        <ResumeDocumentSection>
+          <SectionContent>
+            <SectionHeading>
+              <SectionHeadingText>My Resume Document</SectionHeadingText>
+              <div>
+                <SectionHeadingIcon>
+                  <HiPencil />
+                </SectionHeadingIcon>
+                <SectionHeadingIcon>
+                  <HiTrash />
+                </SectionHeadingIcon>
+              </div>
+            </SectionHeading>
+            <SectionBody>
+              <UploadedResumeContainer>
+                <h4>SMG-CV.pdf</h4>
+                <SectionHeadingIcon>
+                  <HiDocumentArrowDown />
+                </SectionHeadingIcon>
+              </UploadedResumeContainer>
+              <LastUpdated>Last updated 13 days ago</LastUpdated>
+            </SectionBody>
+          </SectionContent>
+        </ResumeDocumentSection>
+        <Label>Summary</Label>
+        <SummarySection>
+          <SectionContent>
+            <SectionHeading>
+              <SummarySectionText>
+                A passionate Self-Taught Developer who aspires to create
+                meaningful projects that gives positive impact to the society.
+                Motivated, problem solver, flexible and eager to learn new tools
+                and technology. Seeking a position as a Developer where my
+                extensive experience will be further developed and utilized.
+              </SummarySectionText>
+              <SectionHeadingIcon>
+                <HiPencil />
+              </SectionHeadingIcon>
+            </SectionHeading>
+          </SectionContent>
+        </SummarySection>
+        <Label>Links</Label>
         <LinksSection>
-          <LinkContainer>LinkedIn</LinkContainer>
-          <LinkContainer>Github</LinkContainer>
+          <LinkContainer>
+            <SectionContent>
+              <SectionHeading>
+                <SummarySectionText>
+                  LinkedIn:
+                  https://www.linkedin.com/in/siegfred-gamboa-xxxxxxxxx/
+                </SummarySectionText>
+                <SectionHeadingIcon>
+                  <HiPencil />
+                </SectionHeadingIcon>
+              </SectionHeading>
+            </SectionContent>
+          </LinkContainer>
+          <LinkContainer>
+            <SectionContent>
+              <SectionHeading>
+                <SummarySectionText>
+                  Github: https://github.com/iamtigermaximus
+                </SummarySectionText>
+                <SectionHeadingIcon>
+                  <HiPencil />
+                </SectionHeadingIcon>
+              </SectionHeading>
+            </SectionContent>
+          </LinkContainer>
         </LinksSection>
-        <h4>Skills</h4>
-        <SkillsSection>Skills section</SkillsSection>
-        <h4>Work Experience</h4>
-        <ExperienceSection>Work experience section</ExperienceSection>
-        <h4>Education</h4>
-        <EducationSection>Education section</EducationSection>
+        <Label>Skills</Label>
+        <SkillsSection>
+          <SkillsWrapper>
+            <SkillContainer>ReactJS</SkillContainer>
+            <SkillContainer>Typescript</SkillContainer>
+            <SkillContainer>NextJS</SkillContainer>
+            <SkillContainer>NodeJS</SkillContainer>
+            <SkillContainer>NodeJS</SkillContainer>
+            <SkillContainer>NodeJS</SkillContainer>
+            <SkillContainer>NodeJS</SkillContainer>
+            <SkillContainer>NodeJS</SkillContainer>
+            <SkillContainer>NodeJS</SkillContainer>
+          </SkillsWrapper>
+        </SkillsSection>
+        <AddLabelContainer>
+          <Label>Work Experience</Label>
+          <AddButton>Add</AddButton>
+        </AddLabelContainer>
+        <ExperienceSection>
+          <WorkSectionContent>
+            <SectionHeading>
+              <SectionHeadingText>
+                <h4>Frontend Developer</h4>
+                <h4>Emmy Clothing Company</h4>
+              </SectionHeadingText>
+              <div>
+                <SectionHeadingIcon>
+                  <HiPencil />
+                </SectionHeadingIcon>
+                <SectionHeadingIcon>
+                  <HiTrash />
+                </SectionHeadingIcon>
+              </div>
+            </SectionHeading>
+            <SectionBody>
+              <h4>Finland</h4>
+              <h4>
+                Debug errors, troubleshoot issues and perform routine
+                performance optimization and usability
+              </h4>
+              <h4>Make suggestions for better solutions to problems</h4>
+              <h4>
+                Collaborate with other team members and discuss possible
+                solutions to issues
+              </h4>
+              <h4>Maintain and improve company's website (webstore)</h4>
+            </SectionBody>
+          </WorkSectionContent>
+          <WorkSectionContent>
+            <SectionHeading>
+              <SectionHeadingText>
+                <h4>Frontend Developer</h4>
+                <h4>Emmy Clothing Company</h4>
+              </SectionHeadingText>
+              <div>
+                <SectionHeadingIcon>
+                  <HiPencil />
+                </SectionHeadingIcon>
+                <SectionHeadingIcon>
+                  <HiTrash />
+                </SectionHeadingIcon>
+              </div>
+            </SectionHeading>
+            <SectionBody>
+              <h4>Finland</h4>
+              <h4>
+                Debug errors, troubleshoot issues and perform routine
+                performance optimization and usability
+              </h4>
+              <h4>Make suggestions for better solutions to problems</h4>
+              <h4>
+                Collaborate with other team members and discuss possible
+                solutions to issues
+              </h4>
+              <h4>Maintain and improve company's website (webstore)</h4>
+            </SectionBody>
+          </WorkSectionContent>
+        </ExperienceSection>
+        <Label>Education</Label>
+        <EducationSection>
+          <WorkSectionContent>
+            <SectionHeading>
+              <SectionHeadingText>
+                <h4>Educational Degree</h4>
+                <h4>University of Helsinki</h4>
+              </SectionHeadingText>
+              <div>
+                <SectionHeadingIcon>
+                  <HiPencil />
+                </SectionHeadingIcon>
+                <SectionHeadingIcon>
+                  <HiTrash />
+                </SectionHeadingIcon>
+              </div>
+            </SectionHeading>
+            <SectionBody>
+              <SectionHeadingIcon>
+                <HiCalendarDays /> 12/21/2023
+              </SectionHeadingIcon>
+              <h4> Helsinki,Finland</h4>
+            </SectionBody>
+          </WorkSectionContent>
+          <WorkSectionContent>
+            <SectionHeading>
+              <SectionHeadingText>
+                <h4>Educational Degree</h4>
+                <h4>University of Helsinki</h4>
+              </SectionHeadingText>
+              <div>
+                <SectionHeadingIcon>
+                  <HiPencil />
+                </SectionHeadingIcon>
+                <SectionHeadingIcon>
+                  <HiTrash />
+                </SectionHeadingIcon>
+              </div>
+            </SectionHeading>
+            <SectionBody>
+              <SectionHeadingIcon>
+                <HiCalendarDays /> 12/21/2023
+              </SectionHeadingIcon>
+              <h4> Helsinki,Finland</h4>
+            </SectionBody>
+          </WorkSectionContent>
+        </EducationSection>
       </ProfileContainer>
     </Container>
   );
