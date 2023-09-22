3c01090f97dab707164f483754b591336856296b
Siegfred Gamboa <siegy.gamboa@gmail.com>
2023-09-22 03:29:26 +0300

    refactor home page, make it responsive to smaller screen

--- src/app/page.styles.tsx
+++ src/app/page.styles.tsx
@@ -1,48 +1,63 @@
 'use client';
 
-import Link from 'next/link';
 import { breakpoints as bp } from '../utils/layout';
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  width: 80vw;
-  height: 100vh;
-  float: left;
-  display: flex;
-  flex-direction: column;
+  width: 100%;
+
+  @media (min-width: ${bp.lg}) {
+    display: flex;
+    width: 80vw;
+    height: 100vh;
+    float: left;
+    flex-direction: column;
+  }
 `;
 
 export const BoxContainer = styled.div`
-  width: 100%;
-  height: 100%;
   display: flex;
-  flex-direction: row;
-  background-color: #b8c1ec;
+  flex-direction: column;
+
+  @media (min-width: ${bp.lg}) {
+    width: 100%;
+    height: 100%;
+    flex-direction: row;
+    background-color: #b8c1ec;
+  }
 `;
 
 export const Box = styled.div`
-  width: 50%;
-  height: 100%;
-  position: relative;
-  padding: 20px;
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
+  width: 100%;
+
+  @media (min-width: ${bp.lg}) {
+    width: 50%;
+    height: 100%;
+    position: relative;
+    padding: 20px;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+  }
 `;
 
 export const Box2 = styled.div`
-  width: 50%;
-  height: 100%;
-  position: relative;
-  padding: 20px;
+  width: 100%;
   display: flex;
   justify-content: center;
-  flex-direction: column;
-  background-image: url('/blobanimation.svg');
-  /* background-size: cover; */
-  background-repeat: no-repeat;
-  background-position: center;
-  /* object-fit: fill; */
+
+  @media (min-width: ${bp.lg}) {
+    width: 50%;
+    height: 100%;
+    position: relative;
+    padding: 20px;
+    flex-direction: column;
+    background-image: url('/blobanimation.svg');
+    /* background-size: cover; */
+    background-repeat: no-repeat;
+    background-position: center;
+    /* object-fit: fill; */
+  }
 `;
 
 export const BoxHeader = styled.div`
