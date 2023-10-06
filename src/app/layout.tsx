6e12b2b19346cd241f323eb62f9f9169591f78b6
Siegfred Gamboa <siegy.gamboa@gmail.com>
2023-10-06 06:41:48 +0300

    refactor layout, added new global font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,14 @@
 import StyledComponentsRegistry from '@/lib/registry';
 import './globals.css';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import Navbar from '@/components/navbar/Navbar';
 import { NextAuthProvider } from './providers/sessionProviders';
+import { Roboto } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] });
+const roboto = Roboto({
+  weight: ['400'],
+  subsets: ['latin'],
+});
 
 export const metadata: Metadata = {
   title: 'JOBLYS',
@@ -19,7 +22,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={roboto.className}>
         <NextAuthProvider>
           <StyledComponentsRegistry>
             <Navbar />
