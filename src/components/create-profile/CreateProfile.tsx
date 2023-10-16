5345c3d6bfc1ab4a7d862bec19b93ef2b6dc80d3
Siegfred Gamboa <siegy.gamboa@gmail.com>
2023-10-16 06:39:20 +0300

    refactor CreateProfile component, rename css files

--- src/components/create-profile/CreateProfile.tsx
+++ src/components/create-profile/CreateProfile.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React from 'react';
-import { Container, CreateProfileContainer } from './CreateProfile.style';
+import { Container, CreateProfileContainer } from './CreateProfile.styles';
 import UploadCvSection from './uploadCvSection/UploadCvSection';
 import BuildProfileSection from './buildProfileSection/BuildProfileSection';
 
