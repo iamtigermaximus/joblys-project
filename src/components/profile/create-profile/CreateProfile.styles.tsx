da78079498029901d322282a7b3fd404f294d6a4
Siegfred Gamboa <siegy.gamboa@gmail.com>
2023-10-17 12:40:06 +0300

    refactor create profile page, added page title

--- src/components/profile/create-profile/CreateProfile.styles.tsx
+++ src/components/profile/create-profile/CreateProfile.styles.tsx
@@ -7,6 +7,7 @@ import styled from 'styled-components';
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
+
   width: 100%;
   padding: 50px;
   overflow-y: scroll;
@@ -16,9 +17,22 @@ export const Container = styled.div`
   }
 `;
 
+export const TitleContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const PageTitle = styled.div`
+  font-size: 25px;
+  color: ${colors.blueGray};
+`;
+
 export const CreateProfileContainer = styled.div`
   display: flex;
   flex-direction: row;
+  justify-content: center;
+  align-items: center;
   background-color: ${colors.white};
   border-radius: 8px;
   box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
--- src/components/profile/create-profile/CreateProfile.tsx
+++ src/components/profile/create-profile/CreateProfile.tsx
@@ -1,15 +1,24 @@
 'use client';
 
 import React from 'react';
-import { Container, CreateProfileContainer } from './CreateProfile.styles';
+import {
+  Container,
+  CreateProfileContainer,
+  PageTitle,
+  TitleContainer,
+} from './CreateProfile.styles';
 import UploadCvSection from './uploadCvSection/UploadCvSection';
 import BuildProfileSection from './buildProfileSection/BuildProfileSection';
 
 const CreateProfile = () => {
   return (
     <Container>
+      <TitleContainer>
+        <PageTitle>Please create your profile</PageTitle>
+      </TitleContainer>
       <CreateProfileContainer>
         <UploadCvSection />
+        <h4>or</h4>
         <BuildProfileSection />
       </CreateProfileContainer>
     </Container>
