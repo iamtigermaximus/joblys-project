5416604514a188019ecfce63bce147c2a2af1df8
Siegfred Gamboa <siegy.gamboa@gmail.com>
2023-11-09 13:07:47 +0200

    refactor middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,23 @@
-export { default } from 'next-auth/middleware';
+// export { default } from 'next-auth/middleware';
 
-export const config = {
-  matcher: ['/joblys/profile', '/joblys/dashboard', '/joblys/cover-letters'],
-};
+// export const config = {
+//   matcher: ['/joblys/profile', '/joblys/dashboard', '/joblys/cover-letters'],
+// };
+
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import { isAuthenticated } from './utils/Auth';
+
+const protectedRoutes = [
+  '/joblys/profile',
+  '/joblys/dashboard',
+  '/joblys/cover-letters',
+  '/joblys/resumes',
+];
+
+export default function middleware(req: NextRequest) {
+  if (!isAuthenticated && protectedRoutes.includes(req.nextUrl.pathname)) {
+    const absoluteURL = new URL('/', req.nextUrl.origin);
+    return NextResponse.redirect(absoluteURL.toString());
+  }
+}
