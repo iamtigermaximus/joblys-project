4f17847a3c77bb935574b2e68686996c26b158ab
Janne Sauvala <janne.sauvala@gmail.com>
2023-12-02 20:56:57 +0200

    Remove old references to 'context'

--- src/app/joblys/resumes/page.tsx
+++ src/app/joblys/resumes/page.tsx
@@ -4,12 +4,8 @@ import React from 'react';
 import { Container } from '../page.styles';
 import PageHeader from '@/components/common/page-header/PageHeader';
 // import Resumes from '@/components/resumes/CreateProfile';
-import { useCv } from '../context';
 
 const ResumesPage = () => {
-  const { cv, setCv } = useCv();
-  console.log('cv from context', cv);
-  
   return (
     <Container>
       <div>
