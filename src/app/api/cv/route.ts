f1bf7e05183667f3c7a9d426b320baa3fc949cb3
Janne Sauvala <janne.sauvala@gmail.com>
2023-12-12 17:52:28 +0200

    Format the promt

--- src/app/api/cv/route.ts
+++ src/app/api/cv/route.ts
@@ -7,7 +7,16 @@ const openAI = new OpenAI({
   apiKey: process.env['OPENAI_API_KEY'],
 });
 
-const parserPromt = 'You will be provided with extracted text from a .docx CV, and your task is to parse it and organize the text. Remove unrealted text regarding the CV. Use the same structure from top to down. Output the data in JSON format. Have the following fields in the top level JSON: name, personal_information, technical_skills, languages, work_experience, personal_projects, education, interest. In the work_experiense, list with different companies and roles as json array with company_name, start_date, end_date, location, and resposibilites, job title, start date, end date. In the education list  school name, location, degree or program, start and end dates. If some field or data is missing or you cannot parse it, mark the field with n/a.';
+const parserPromt = `You will be provided with extracted text from a .docx CV, and your task is to parse it and organize the text. Remove unrealted text regarding the CV. Use the same structure from top to down. Output the data in JSON format. Have the following fields in the top level JSON:
+- name (the CV holder's name)
+- personal_information, and under it keys email, phone_number, about_me
+- technical_skills (as an array of skills as strings)
+- languages (as an array of strings)
+- work_experience, and under it as an array of objects with keys: company_name (string), position (job title, string), location (location of the company, string), start_date (string), end_date (string), responsibilities (array of strings)
+- personal_projects, and under it as an array of objects with keys: name (string), start_date (string), end_date (string)
+- education, and under it as an array of objcts with keys: degree (string), location (string), start_date (string), end_date (string), grade (string)
+- interests (array of strings).
+If some field or data is missing or you cannot parse it, mark the field with n/a.`;
 
 export async function POST(req: NextRequest, res: NextResponse) {
   const token = await getToken({ req })
