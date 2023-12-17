cce0f604faa4fe2ab6a0b70d34fdebf3c5be312a
Siegfred Gamboa <siegy.gamboa@gmail.com>
2023-12-18 00:11:36 +0200

    refactor build-profile page link to profile-builder

--- src/components/profile/create-profile/buildProfileSection/BuildProfileSection.tsx
+++ src/components/profile/create-profile/buildProfileSection/BuildProfileSection.tsx
@@ -15,7 +15,7 @@ const BuildProfileSection = () => {
   const router = useRouter();
 
   const handleBuildProfile = () => {
-    router.push('/joblys/profile/build-profile');
+    router.push('/joblys/profile-builder');
   };
   return (
     <CreateProfileSection>
