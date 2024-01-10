68d3932ac6765026308499cd88395e73265e0126
Janne Sauvala <janne.sauvala@gmail.com>
2024-01-10 21:59:56 +0200

    Show earlier parsed resume in the builder

---
+++ src/types/storedResume.ts
@@ -0,0 +1,12 @@
+export interface WorkExperience {
+  position: string;
+  company_name: string;
+  start_date: string;
+  end_date: string;
+  responsibilities: string[];
+}
+
+export interface Education {
+  start_date: string;
+  end_date: string;
+}
