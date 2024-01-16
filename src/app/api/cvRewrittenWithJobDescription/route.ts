9862845c3f710985c804bd5f250435b254354cd0
linh <bmquynlinh@gmail.com>
2024-01-16 21:57:43 +0200

    add cvRewrittenWithJD

--- src/app/api/cvRewrittenWithJobDescription/route.ts
+++ src/app/api/cvRewrittenWithJobDescription/route.ts
@@ -2,19 +2,21 @@ import { NextRequest, NextResponse } from 'next/server';
 import prisma from '../../../lib/prisma';
 import OpenAI from 'openai';
 
-const parserPromt = `Please rewrite the following three job responsibilities to enhance their professional appeal for a CV, while preserving their original meaning.
-Each rewritten responsibility should be a more professional and concise version of the original, suitable for a CV. List your responsibilities in a bullet or numbered format, ensuring that the essence of each task remains the same, without introducing new facts or figures.
-Each rewritten responsibility should correspond directly to the original ones provided, maintaining a concise length suitable for a CV. Focus on using a variety of dynamic action verbs and professional terminology, especially if your experience is in a specific industry. For clarity, see the examples provided after the original responsibilities. 
-Please provide exactly {number_sentences} rewritten sentences, one corresponding to each of the following original responsibilities:
+const parserPromt = `
+I am in the process of updating my resume for a new job opportunity and need assistance in rephrasing my current job responsibilities. The goal is to align them more closely with the position I am aiming for. Each rewritten sentence should be concise yet impactful, containing between 10 to 50 words. Emphasis should be on dynamic action verbs and industry-specific terminology to enhance my resume's appeal for the new role.
 
-Original Responsibilities:
+Please rewrite each of my job responsibilities, listed below, into a single sentence that adheres to the specified word count. These rephrasings should mirror the skills and requirements of the new role I am applying for, creating a strong alignment with my experience and the job's expectations.
+
+Current Job Responsibilities:
 {original_responsibilities}
-Focus on using a variety of dynamic action verbs and professional terminology, especially if your experience is in a specific industry (please mention if so). For clarity, see the examples below:
 
-Developed and optimized back-end APIs for a large-scale e-commerce platform, leading to significant improvements in response time.
-Designed and executed a comprehensive digital marketing strategy, substantially increasing online brand presence and social media engagement.
-Oversaw and directed multiple high-priority projects, ensuring completion within established timeframes and budget constraints.
-Led cross-functional teams to foster collaboration and effective communication, successfully meeting project milestones.`;
+Target Job Description for the New Role:
+{job_description}
+
+Your assistance in providing these tailored, succinct rephrased sentences will be greatly appreciated.
+
+
+`;
 
 interface ResumeData {
   'Work Experience': WorkExperience;
@@ -37,13 +39,22 @@ const openAI = new OpenAI({
 });
 
 export async function POST(req: NextRequest) {
-  const { id } = await req.json();
+  const { id, job_descriptions } = await req.json();
 
   if (!id) {
     return NextResponse.json(
       {
         body: {
           message: 'ID not provided',
+        },
+      },
+      { status: 400 },
+    );
+  } else if (!job_descriptions) {
+    return NextResponse.json(
+      {
+        body: {
+          message: 'Job description not provided',
         },
       },
       { status: 400 },
@@ -86,24 +97,28 @@ export async function POST(req: NextRequest) {
   const formattedResponsibilities = allResponsibilities
     .map(responsibility => `- ${responsibility}`)
     .join('\n');
-
   const replacementMap: Record<string, string> = {
-    '{number_sentences}': allResponsibilities.length.toString(),
     '{original_responsibilities}': formattedResponsibilities,
+    '{job_description}': job_descriptions,
   };
   const modifiedPrompt = parserPromt.replace(
-    /{original_responsibilities}|{number_sentences}/g,
+    /{original_responsibilities}|{job_description}/g,
     match => replacementMap[match],
   );
 
-  const openaiResponse = await openAI.completions.create({
-    model: 'gpt-3.5-turbo-instruct',
-    prompt: modifiedPrompt,
+  const openaiResponse = await openAI.chat.completions.create({
+    model: 'gpt-4',
+    messages: [
+      {
+        role: 'user',
+        content: modifiedPrompt,
+      },
+    ],
     max_tokens: allResponsibilities.length * 30,
     temperature: 0.5,
   });
 
-  const responseContent = openaiResponse?.choices?.[0]?.text;
+  const responseContent = openaiResponse?.choices?.[0]?.message?.content;
   if (responseContent === undefined || responseContent === null) {
     return NextResponse.json(
       {
