212dc51b237ce21d582b38ceee51169be84e00e6
linh <bmquynlinh@gmail.com>
2024-01-28 00:37:24 +0200

    reformat file

--- src/app/api/cvRewrittenWithJobDescription/route.ts
+++ src/app/api/cvRewrittenWithJobDescription/route.ts
@@ -172,7 +172,7 @@ export async function POST(req: NextRequest) {
       },
       { status: 500 },
     );
-  } 
+  }
   const rewrittenResponsibilities = responseContent
     .trim()
     .split('\n')
