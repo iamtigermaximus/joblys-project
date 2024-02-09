6e2e060464efeafb649e56fa817fb190f2335503
Siegfred Gamboa <siegy.gamboa@gmail.com>
2024-02-09 14:08:19 +0200

    refactor default template component, added styling and created layout of the template

---
+++ src/components/templates/defaultTemplate/DefaultTemplate.styles.tsx
@@ -0,0 +1,202 @@
+import styled from 'styled-components';
+import { breakpoints as bp } from '../../../utils/layout';
+import colors from '../../../utils/colors';
+
+import {
+  FaUser,
+  FaEnvelope,
+  FaPhone,
+  FaHome,
+  FaLinkedin,
+  FaGlobe,
+} from 'react-icons/fa';
+
+export const DefaultTemplateContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  padding: 20px 30px;
+  overflow-y: auto;
+  min-height: 800px;
+`;
+
+export const Template = styled.div`
+  display: flex;
+  flex-direction: row;
+  background-color: white;
+  width: 100%;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  margin-bottom: 20px;
+  flex-grow: 1;
+
+  &.clicked {
+    transform: scale(0.7);
+    transition: transform 0.3s ease;
+  }
+`;
+export const BasicContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: #232946;
+  width: 35%;
+  padding: 50px 20px;
+  color: white;
+`;
+
+export const ContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: #fffffe;
+  width: 65%;
+  padding: 50px 20px;
+`;
+
+export const BasicsTitleContainer = styled.div`
+  display: flex;
+  padding-bottom: 5px;
+`;
+
+export const BasicsTitle = styled.h1`
+  color: white;
+  font-size: 15px;
+`;
+
+export const BasicsNameContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 10px;
+`;
+
+export const BasicsItemsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 10px 0;
+`;
+export const BasicsItem = styled.h1`
+  color: white;
+  font-size: 12px;
+  max-width: 100%;
+  word-break: break-all;
+  white-space: wrap;
+  padding: 5px 0;
+`;
+
+export const IconContainer = styled.span`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-right: 5px;
+  font-size: 17px;
+`;
+
+export const HeaderName = styled.h1`
+  color: #232946;
+  font-size: 30px;
+  max-width: 100%;
+  word-break: break-all;
+  white-space: wrap;
+  padding: 3px 0;
+`;
+
+export const HeaderCurrentRole = styled.h1`
+  color: #232946;
+  font-size: 15px;
+  max-width: 100%;
+  word-break: break-all;
+  white-space: wrap;
+`;
+export const DetailsTitleContainer = styled.div`
+  display: flex;
+  padding: 5px 0;
+`;
+export const DetailsTitle = styled.h1`
+  color: #232946;
+  font-size: 15px;
+  border-bottom: 0.5px solid #232946;
+`;
+
+export const SummaryContainer = styled.div`
+  display: flex;
+  width: 100%;
+`;
+
+export const Summary = styled.h1`
+  color: #232946;
+  font-size: 12px;
+  max-width: 100%;
+  word-break: break-all;
+  white-space: wrap;
+  padding: 5px 0;
+`;
+
+export const DetailsContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  font-size: 12px;
+  padding: 5px 0;
+`;
+
+export const EmploymentDetailsContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const EmploymentDetail = styled.div``;
+export const DateContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  width: 100%;
+`;
+
+export const Dates = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 5px;
+`;
+export const Month = styled.div``;
+export const Year = styled.div``;
+export const DateSeparator = styled.div`
+  font-size: 14px;
+  padding: 0 5px;
+`;
+export const JobTitle = styled.div`
+  width: 100%;
+  font-weight: 700;
+  white-space: nowrap;
+  font-size: 13px;
+`;
+export const Company = styled.div`
+  width: 100%;
+  font-weight: 700;
+  white-space: nowrap;
+  font-size: 13px;
+`;
+export const Description = styled.div`
+  padding: 5px 0;
+`;
+
+export const EducationDetailContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const Course = styled.div`
+  width: 100%;
+  font-weight: 700;
+  white-space: nowrap;
+  font-size: 13px;
+`;
+
+export const School = styled.div`
+  width: 100%;
+  font-weight: 700;
+  white-space: nowrap;
+  font-size: 13px;
+`;
+
+export const EducationDetail = styled.div``;
--- src/components/templates/defaultTemplate/DefaultTemplate.tsx
+++ src/components/templates/defaultTemplate/DefaultTemplate.tsx
@@ -1,65 +1,52 @@
 'use client';
-
 import React, { FC, useState } from 'react';
 import { ResumeInfoType } from '@/types/profile';
-import styled from 'styled-components';
-import { breakpoints as bp } from '../../../utils/layout';
-import colors from '../../../utils/colors';
-
-export const DefaultTemplateContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 100%;
-  height: 100%;
-  padding: 20px;
-  overflow-y: auto;
-
-  @media (min-width: ${bp.md}) {
-    padding: 20px 100px;
-  }
-
-  @media (min-width: ${bp.lg}) {
-    padding: 20px 30px;
-    height: 100%;
-  }
-`;
-
-export const Template = styled.div`
-  display: flex;
-  flex-direction: row;
-  background-color: white;
-  width: 100%;
-  height: 100vh;
-  border: 1px solid green;
-
-  &.clicked {
-    transform: scale(0.7);
-    transition: transform 0.3s ease;
-  }
-`;
-export const BasicContentContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  background-color: #232946;
-  width: 35%;
-  height: 100%;
-  padding: 50px 20px;
-  color: white;
-`;
-
-export const ContentContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  background-color: #fffffe;
-  width: 65%;
-  height: 100%;
-  padding: 50px 20px;
-`;
+import {
+  FaUser,
+  FaEnvelope,
+  FaPhone,
+  FaHome,
+  FaLinkedin,
+  FaGlobe,
+} from 'react-icons/fa';
+import {
+  DefaultTemplateContainer,
+  Template,
+  BasicContentContainer,
+  BasicsTitleContainer,
+  BasicsTitle,
+  BasicsItemsContainer,
+  BasicsNameContainer,
+  IconContainer,
+  BasicsItem,
+  ContentContainer,
+  HeaderName,
+  HeaderCurrentRole,
+  DetailsTitleContainer,
+  DetailsTitle,
+  SummaryContainer,
+  Summary,
+  DetailsContentContainer,
+  EmploymentDetailsContainer,
+  EmploymentDetail,
+  JobTitle,
+  Company,
+  DateContainer,
+  Dates,
+  Month,
+  Year,
+  DateSeparator,
+  Description,
+  EducationDetailContainer,
+  EducationDetail,
+  Course,
+  School,
+} from './DefaultTemplate.styles';
 
 interface DefaultTemplateProps {
   resumeInfo: ResumeInfoType;
 }
+
 const DefaultTemplate: FC<DefaultTemplateProps> = ({ resumeInfo }) => {
   const basic = resumeInfo?.basic;
   const professional = resumeInfo?.professional;
@@ -72,12 +59,254 @@ const DefaultTemplate: FC<DefaultTemplateProps> = ({ resumeInfo }) => {
     setIsClicked(!isClicked);
   };
 
+  const shouldDisplayTitle =
+    !!(
+      basic.firstName ||
+      basic.lastName ||
+      basic.email ||
+      basic.phoneNumber ||
+      basic.address ||
+      basic.linkedin ||
+      (basic.additionalLinks && basic.additionalLinks.length > 0)
+    ) ||
+    (skills && skills.skill && skills.skill.length > 1) ||
+    (languages && languages.language && languages.language.length > 1) ||
+    !!(
+      (professional.work && professional.work.length > 1) ||
+      professional.summary
+    ) ||
+    !!(educational.education && educational.education.length > 1);
+
   const templateClassName = isClicked ? 'clicked' : '';
+
+  // Debugging: Log skills and languages
+  console.log('Skills:', skills);
+  console.log('Languages:', languages);
+
   return (
     <DefaultTemplateContainer>
       <Template>
-        <BasicContentContainer></BasicContentContainer>
-        <ContentContainer></ContentContainer>
+        <BasicContentContainer>
+          <BasicsTitleContainer>
+            {shouldDisplayTitle && <BasicsTitle>Personal Details</BasicsTitle>}
+          </BasicsTitleContainer>
+          <BasicsItemsContainer>
+            {basic.firstName && (
+              <BasicsNameContainer>
+                <IconContainer>
+                  <FaUser />
+                </IconContainer>
+                <BasicsItem>{basic.firstName}</BasicsItem>
+                <BasicsItem>{basic.lastName}</BasicsItem>
+              </BasicsNameContainer>
+            )}
+            {basic.email && (
+              <BasicsNameContainer>
+                <IconContainer>
+                  <FaEnvelope />
+                </IconContainer>
+                <BasicsItem>{basic.email}</BasicsItem>
+              </BasicsNameContainer>
+            )}
+            {basic.phoneNumber && (
+              <BasicsNameContainer>
+                <IconContainer>
+                  <FaPhone />
+                </IconContainer>
+                <BasicsItem>{basic.phoneNumber}</BasicsItem>
+              </BasicsNameContainer>
+            )}
+            {basic.address && (
+              <BasicsNameContainer>
+                <IconContainer>
+                  <FaHome />
+                </IconContainer>
+                <BasicsItem>{basic.address}</BasicsItem>
+              </BasicsNameContainer>
+            )}
+            {basic.linkedin && (
+              <BasicsNameContainer>
+                <IconContainer>
+                  <FaLinkedin />
+                </IconContainer>
+                <BasicsItem>{basic.linkedin}</BasicsItem>
+              </BasicsNameContainer>
+            )}
+            {basic?.additionalLinks.map(link => (
+              <BasicsNameContainer key={link.id}>
+                <IconContainer>
+                  <FaGlobe />
+                </IconContainer>
+                <a href={link.url}>
+                  <BasicsItem>{link.url}</BasicsItem>
+                </a>
+              </BasicsNameContainer>
+            ))}
+          </BasicsItemsContainer>
+
+          {shouldDisplayTitle && (
+            <>
+              <BasicsTitleContainer> Skills</BasicsTitleContainer>
+
+              {skills.skill &&
+                skills?.skill.map(enteredSkill => (
+                  <BasicsItem key={enteredSkill.id}>
+                    <h3>{enteredSkill.name}</h3>
+                  </BasicsItem>
+                ))}
+              <BasicsTitleContainer> Languages</BasicsTitleContainer>
+              {languages.language &&
+                languages?.language.map(enteredLanguage => (
+                  <BasicsItem key={enteredLanguage.id}>
+                    <h3>{enteredLanguage.name}</h3>
+                  </BasicsItem>
+                ))}
+            </>
+          )}
+        </BasicContentContainer>
+        <ContentContainer>
+          <BasicsNameContainer>
+            <HeaderName>{basic.firstName}</HeaderName>
+            <HeaderName>{basic.lastName}</HeaderName>
+          </BasicsNameContainer>
+          <HeaderCurrentRole>{professional.currentRole}</HeaderCurrentRole>
+          <DetailsTitleContainer>
+            {shouldDisplayTitle && <DetailsTitle>Summary</DetailsTitle>}
+          </DetailsTitleContainer>
+          <SummaryContainer>
+            <Summary>{professional.summary}</Summary>
+          </SummaryContainer>
+          <DetailsTitleContainer>
+            {shouldDisplayTitle && <DetailsTitle>Employment</DetailsTitle>}
+          </DetailsTitleContainer>
+          {professional.work && (
+            <DetailsContentContainer>
+              {professional.work.map(experience => (
+                <div key={experience.id}>
+                  {experience.jobTitle && (
+                    <EmploymentDetailsContainer>
+                      <EmploymentDetail>
+                        <JobTitle>{experience.jobTitle}</JobTitle>
+                        <Company>{experience.company}</Company>
+                      </EmploymentDetail>
+                      <DateContainer>
+                        <Dates>
+                          <Month>
+                            {experience.startDate.month &&
+                            !isNaN(parseInt(experience.startDate.month)) ? (
+                              <>
+                                {new Date(
+                                  2022,
+                                  parseInt(experience.startDate.month) - 1,
+                                ).toLocaleString('default', {
+                                  month: 'short',
+                                })}
+                              </>
+                            ) : (
+                              <>Jan</>
+                            )}
+                          </Month>
+                          <Year>
+                            {experience.startDate.year ||
+                              new Date().getFullYear()}
+                          </Year>
+                        </Dates>
+                        <DateSeparator> - </DateSeparator>
+                        <Dates>
+                          <Month>
+                            {experience.endDate.month &&
+                            !isNaN(parseInt(experience.endDate.month)) ? (
+                              <>
+                                {new Date(
+                                  2022,
+                                  parseInt(experience.endDate.month) - 1,
+                                ).toLocaleString('default', {
+                                  month: 'short',
+                                })}
+                              </>
+                            ) : (
+                              <>Jan</>
+                            )}
+                          </Month>
+                          <Year>
+                            {experience.endDate.year ||
+                              new Date().getFullYear()}
+                          </Year>
+                        </Dates>
+                      </DateContainer>
+                    </EmploymentDetailsContainer>
+                  )}
+
+                  <Description>{experience.jobDetails}</Description>
+                </div>
+              ))}
+            </DetailsContentContainer>
+          )}
+
+          <DetailsTitleContainer>
+            {shouldDisplayTitle && <DetailsTitle>Education</DetailsTitle>}
+          </DetailsTitleContainer>
+          <DetailsContentContainer>
+            {educational.education.map(educ => (
+              <div key={educ.id}>
+                {educ.school && (
+                  <EducationDetailContainer>
+                    <EducationDetail>
+                      <Course>{educ.course}</Course>
+                      <School>{educ.school}</School>
+                    </EducationDetail>
+                    <DateContainer>
+                      <Dates>
+                        <Month>
+                          {educ.startDate.month &&
+                          !isNaN(parseInt(educ.startDate.month)) ? (
+                            <>
+                              {new Date(
+                                2022,
+                                parseInt(educ.startDate.month) - 1,
+                              ).toLocaleString('default', {
+                                month: 'short',
+                              })}
+                            </>
+                          ) : (
+                            <>Jan</>
+                          )}
+                        </Month>
+                        <Year>
+                          {educ.startDate.year || new Date().getFullYear()}
+                        </Year>
+                      </Dates>
+                      <DateSeparator> - </DateSeparator>
+
+                      <Dates>
+                        <Month>
+                          {educ.endDate.month &&
+                          !isNaN(parseInt(educ.endDate.month)) ? (
+                            <>
+                              {new Date(
+                                2022,
+                                parseInt(educ.endDate.month) - 1,
+                              ).toLocaleString('default', {
+                                month: 'short',
+                              })}
+                            </>
+                          ) : (
+                            <>Jan</>
+                          )}
+                        </Month>
+                        <Year>
+                          {educ.endDate.year || new Date().getFullYear()}
+                        </Year>
+                      </Dates>
+                    </DateContainer>
+                  </EducationDetailContainer>
+                )}
+
+                <Description>{educ.description}</Description>
+              </div>
+            ))}
+          </DetailsContentContainer>
+        </ContentContainer>
       </Template>
     </DefaultTemplateContainer>
   );
