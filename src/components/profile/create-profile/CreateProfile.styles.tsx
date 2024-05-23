9c353ba4d57e9b5f8c6c69ee172df00b3deb7a61
Siegfred Gamboa <siegy.gamboa@gmail.com>
2024-05-23 13:06:56 +0300

    style: Refactor Create Profile component, put all styled components in sty les files, removed unused codes

--- src/components/profile/create-profile/CreateProfile.styles.tsx
+++ src/components/profile/create-profile/CreateProfile.styles.tsx
@@ -34,6 +34,7 @@ export const ProfileContainer = styled.div`
   height: 100vh;
   align-items: center;
   /* background-color: pink; */
+  padding: 20px 20px 100px;
 
   @media (min-width: ${bp.md}) {
     /* padding: 50px; */
@@ -92,4 +93,210 @@ export const ProfileFormContainer = styled.div`
   /* border: 1px solid red; */
   width: 100%;
   /* background-color: pink; */
+`;
+
+//STYLES FOR READ-ONLY FORM
+
+export const AccordionSection = styled.div`
+  margin-bottom: 10px;
+  width: 100%;
+`;
+
+export const AccordionHeader = styled.div`
+  cursor: pointer;
+  background-color: ${colors.white};
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  border-bottom: 1px solid #f5f5f5;
+
+  height: 50px;
+  padding: 4px;
+  align-items: center;
+  width: 100%;
+`;
+
+export const AccordionHeaderTitle = styled.h1`
+  font-weight: 900;
+  font-size: 20px;
+  color: black;
+  font-family: 'Roboto Rounded', sans-serif;
+`;
+export const AccordionContent = styled.div`
+  /* padding: 5px 0; */
+  /* padding: 4px; */
+`;
+
+export const IconContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const BasicDetailsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: ${colors.white};
+  width: 100%;
+  color: ${colors.white};
+`;
+
+export const InputRow = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding: 4px;
+
+  @media (min-width: ${bp.md}) {
+    flex-direction: row;
+    gap: 20px;
+  }
+`;
+
+export const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding: 4px;
+`;
+
+export const Input = styled.input`
+  border-radius: 5px;
+  padding: 10px;
+  font-size: 14px;
+  background-color: #f5f5f5;
+  border: none;
+  color: black;
+  height: 40px;
+  margin-bottom: 10px;
+  padding: 8px 12px;
+  width: 100%;
+
+  &:focus {
+    outline: 0.5px solid gray;
+  }
+
+  @media (min-width: ${bp.md}) {
+    background-color: #f5f5f5;
+    border: none;
+    color: black;
+    height: 40px;
+    margin-bottom: 10px;
+    padding: 8px 12px;
+    width: 100%;
+
+    &:focus {
+      outline: 0.5px solid gray;
+    }
+  }
+`;
+
+export const InputLabel = styled.label`
+  font-size: 14px;
+  color: ${colors.darkPurple};
+  letter-spacing: 1px;
+  padding: 5px 0;
+
+  @media (min-width: ${bp.md}) {
+    margin: 5px 0;
+  }
+`;
+
+export const NewLinkContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
+  padding: 4px;
+`;
+
+export const EducationalDetailsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: ${colors.white};
+  width: 100%;
+  flex-direction: column;
+  color: ${colors.white};
+`;
+
+export const EducationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+export const ProfessionalDetailsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: ${colors.white};
+  width: 100%;
+  margin-bottom: 20px;
+  flex-direction: column;
+  color: ${colors.white};
+`;
+export const ProfessionalContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+export const SkillsDetailsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: ${colors.white};
+  width: 100%;
+  color: black;
+`;
+
+export const SkillsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+export const LanguagesDetailsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: ${colors.white};
+  width: 100%;
+  color: black;
+`;
+
+export const LanguagesContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+export const TextArea = styled.textarea`
+  border-radius: 5px;
+  font-size: 14px;
+  height: 100px;
+  padding: 8px 12px;
+  background-color: #f5f5f5;
+  color: black;
+  border: none;
+  margin-bottom: 10px;
+  width: 100%;
+
+  &:focus {
+    outline: 0.5px solid gray;
+  }
+
+  @media (min-width: ${bp.md}) {
+    border: none;
+    margin-bottom: 20px;
+  }
+`;
+
+export const DateInfoContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  gap: 10px;
+`;
+
+export const ItemContainer = styled.div`
+  padding: 4px;
+  width: 100%;
 `;
--- src/components/profile/create-profile/CreateProfile.tsx
+++ src/components/profile/create-profile/CreateProfile.tsx
@@ -2,226 +2,42 @@
 
 import React, { useEffect, useState } from 'react';
 import {
+  AccordionContent,
+  AccordionHeader,
+  AccordionHeaderTitle,
+  AccordionSection,
+  BasicDetailsContainer,
   Container,
   CreateProfileContainer,
+  DateInfoContainer,
+  EducationContainer,
+  EducationalDetailsContainer,
+  IconContainer,
+  Input,
+  InputContainer,
+  InputLabel,
+  InputRow,
+  ItemContainer,
+  LanguagesContainer,
+  LanguagesDetailsContainer,
+  NewLinkContainer,
   PageTitle,
+  ProfessionalContainer,
+  ProfessionalDetailsContainer,
   ProfileContainer,
   ProfileFormContainer,
+  SkillsContainer,
+  SkillsDetailsContainer,
+  TextArea,
   TitleContainer,
 } from './CreateProfile.styles';
 import { FaCircleChevronDown, FaCircleChevronUp } from 'react-icons/fa6';
 import UploadCvSection from './uploadCvSection/UploadCvSection';
 import BuildProfileSection from './buildProfileSection/BuildProfileSection';
 import axios from 'axios';
-import styled from 'styled-components';
-import { breakpoints as bp } from '@/utils/layout';
-import colors from '@/utils/colors';
 import { Profile } from '@/types/profile';
 import Loader from '@/components/common/loader/Loader';
 
-export const AccordionSection = styled.div`
-  margin-bottom: 10px;
-  width: 100%;
-`;
-
-export const AccordionHeader = styled.div`
-  cursor: pointer;
-  background-color: ${colors.white};
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  border-bottom: 1px solid #f5f5f5;
-
-  height: 50px;
-  padding: 4px;
-  align-items: center;
-  width: 100%;
-`;
-
-export const AccordionHeaderTitle = styled.h1`
-  font-weight: 900;
-  font-size: 20px;
-  color: black;
-  font-family: 'Roboto Rounded', sans-serif;
-`;
-export const AccordionContent = styled.div`
-  /* padding: 5px 0; */
-  /* padding: 4px; */
-`;
-
-export const IconContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-export const BasicDetailsContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  background-color: ${colors.white};
-  width: 100%;
-  color: ${colors.white};
-`;
-
-export const InputRow = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  padding: 4px;
-
-  @media (min-width: ${bp.md}) {
-    flex-direction: row;
-    gap: 20px; /* Adjust the gap between input fields as needed */
-  }
-`;
-
-export const InputContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  padding: 4px;
-`;
-
-export const Input = styled.input`
-  border-radius: 5px;
-  padding: 10px;
-  font-size: 14px;
-  background-color: #f5f5f5;
-  border: none;
-  color: black;
-  height: 40px;
-  margin-bottom: 10px;
-  padding: 8px 12px;
-  width: 100%;
-
-  &:focus {
-    outline: 0.5px solid gray;
-  }
-
-  @media (min-width: ${bp.md}) {
-    background-color: #f5f5f5;
-    border: none;
-    color: black;
-    height: 40px;
-    margin-bottom: 10px;
-    padding: 8px 12px;
-    width: 100%;
-
-    &:focus {
-      outline: 0.5px solid gray;
-    }
-  }
-`;
-
-export const InputLabel = styled.label`
-  font-size: 14px;
-  color: ${colors.darkPurple};
-  letter-spacing: 1px;
-  padding: 5px 0;
-
-  @media (min-width: ${bp.md}) {
-    margin: 5px 0;
-  }
-`;
-
-export const NewLinkContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  width: 100%;
-  padding: 4px;
-`;
-
-export const EducationalDetailsContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  background-color: ${colors.white};
-  width: 100%;
-  flex-direction: column;
-  color: ${colors.white};
-`;
-
-export const EducationContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-`;
-
-export const ProfessionalDetailsContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  background-color: ${colors.white};
-  width: 100%;
-  margin-bottom: 20px;
-  flex-direction: column;
-  color: ${colors.white};
-`;
-export const ProfessionalContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-`;
-
-export const SkillsDetailsContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  background-color: ${colors.white};
-  width: 100%;
-  color: black;
-`;
-
-export const SkillsContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-`;
-
-export const LanguagesDetailsContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  background-color: ${colors.white};
-  width: 100%;
-  color: black;
-`;
-
-export const LanguagesContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-`;
-
-export const TextArea = styled.textarea`
-  border-radius: 5px;
-  font-size: 14px;
-  height: 100px;
-  padding: 8px 12px;
-  background-color: #f5f5f5;
-  color: black;
-  border: none;
-  margin-bottom: 10px;
-  width: 100%;
-
-  &:focus {
-    outline: 0.5px solid gray;
-  }
-
-  @media (min-width: ${bp.md}) {
-    border: none;
-    margin-bottom: 20px;
-  }
-`;
-
-export const DateInfoContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  width: 100%;
-  gap: 10px;
-`;
-
-export const ItemContainer = styled.div`
-  padding: 4px;
-  width: 100%;
-`;
 const CreateProfile = () => {
   const [existingData, setExistingData] = useState<Profile | null>(null);
   const [isLoading, setIsLoading] = useState(true);
